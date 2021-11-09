d55d429f974a0a8789558bcb16841a1188cdff44
flamesheep <whyicannot28@gmail.com>
2021-11-09 15:38:08 +0800

    211109
    增加物體碰撞、角色動畫

--- KG_2/Assets/Scripts/PlayerContorller.cs
+++ KG_2/Assets/Scripts/PlayerContorller.cs
@@ -5,20 +5,21 @@ using UnityEngine;
 public class PlayerContorller : MonoBehaviour
 {
     public float moveSpeed;
+    public Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 rubyMove = transform.position;
-        rubyMove.x = rubyMove.x + Input.GetAxis("Horizontal") * moveSpeed;
-        rubyMove.y = rubyMove.y + Input.GetAxis("Vertical") * moveSpeed;
-        transform.position = rubyMove;
-        
+        Vector2 rubyPosition = transform.position;
+        rubyPosition.x = rubyPosition.x + Input.GetAxis("Horizontal") * moveSpeed;
+        rubyPosition.y = rubyPosition.y + Input.GetAxis("Vertical") * moveSpeed;
+        //transform.position = rubyMove;
+        rb.MovePosition(rubyPosition);
     }
 }
