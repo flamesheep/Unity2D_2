a6cf959616d9ad163b3e6685b6c0152d4cd03df2
flamesheep <whyicannot28@gmail.com>
2021-11-23 16:09:44 +0800

    211123
    新增攝影機、移動動畫，移動的程式碼

--- KG_2/Assets/Scripts/PlayerContorller.cs
+++ KG_2/Assets/Scripts/PlayerContorller.cs
@@ -4,22 +4,45 @@ using UnityEngine;
 
 public class PlayerContorller : MonoBehaviour
 {
+    private Vector2 lookDirection;
+    private Vector2 rubyPosition;
+    private Vector2 rubyMove;
+
     public float moveSpeed;
+
     public Rigidbody2D rb;
+    public Animator rubyAnimator;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        rubyAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        Vector2 rubyPosition = transform.position;
-        rubyPosition.x = rubyPosition.x + Input.GetAxis("Horizontal") * moveSpeed;
-        rubyPosition.y = rubyPosition.y + Input.GetAxis("Vertical") * moveSpeed;
+        rubyPosition = transform.position;
+
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+
+        rubyMove = new Vector2(moveX, moveY);
+
+        if (!Mathf.Approximately(rubyMove.x, 0) || !Mathf.Approximately(rubyMove.y, 0))
+        {
+            lookDirection = rubyMove;
+            lookDirection.Normalize();
+
+        }
+
+        rubyAnimator.SetFloat("Look X", lookDirection.x);
+        rubyAnimator.SetFloat("Look Y", lookDirection.y);
+        rubyAnimator.SetFloat("Speed", rubyMove.magnitude);
+
         //transform.position = rubyMove;
+        rubyPosition = rubyPosition + moveSpeed * rubyMove * Time.deltaTime;
         rb.MovePosition(rubyPosition);
     }
 }
